fc2aea95a1c2ea5b9efe5059cccfaa0f7f30c531
worsleyj <josephworsley23@gmail.com>
2024-11-11 13:07:36 -0500

    add /new

--- routes/router.js
+++ routes/router.js
@@ -4,4 +4,10 @@ const router = Router();
 router.get("/", (req, res) => {
   res.send("usernames will be logged here");
 });
+router.get("/new", (req, res) => {
+  res.render("new");
+});
+router.post("/new", (req, res) => {
+  console.log("username to be saved : ", req.body.username);
+});
 module.exports = router;
