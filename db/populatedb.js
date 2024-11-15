ae9452b8eb450f8c9bc2c6978246993afe9b8a63
worsleyj <worsleyj23@gmail.com>
2024-11-14 22:55:07 -0500

    fix login for populate script

--- db/populatedb.js
+++ db/populatedb.js
@@ -18,7 +18,7 @@ VALUES
 async function main() {
   console.log("seeding...");
   const client = new Client({
-    connectionString: "postgresql://postgres:postgre@localhost:5432/top_users",
+    connectionString: "postgresql://postgres:postgres@localhost:5432/top_users",
   });
   await client.connect();
   await client.query(SQL);
